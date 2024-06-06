af48046d52b30574293f184682bf10c8b13f080a
WMBarbosa <wesleymario01@gmail.com>
2024-06-06 15:14:23 -0300

    Commit update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n==== TEST 5: seller update ====");
+        seller = sellerDao.findByID(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
     }
 }
